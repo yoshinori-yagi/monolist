1c0e45a0442f12eec82bf1c3b63804c692dc0e49
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-06-15 07:00:09 +0000

    init

---
+++ exam.php
@@ -0,0 +1,10 @@
+<?php
+function showMsgBox($message) {
+  $len = strlen($message);   // strlen() gets string length.
+  // TODO: write your code below
+    return ' '.$this->len.' ';
+  // TODO: write your code above
+}
+
+showMsgBox('This is a sample.');
+?>
